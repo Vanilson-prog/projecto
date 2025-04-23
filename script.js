2c0dbc64b62af75e0658d3d17565c9868135a5bb
Vanilson Magas <fredikvany@gmail.com>
2025-04-23 10:12:18 +0100

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+   const html =document.documentElement
+   html.classList.toggle("light")
+   /*const img= document.querySelector("#profile img")
+
+    if(html.classList.contains('light')){
+        img.setAttribute('src', "./avatar-light.jpg")
+        
+
+    }
+        
+
+else{
+    img.setAttribute("src','./avatar.png")
+    
+}*/
+
+}
